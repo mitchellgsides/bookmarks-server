e4c33b0cbd1678c8cc8fe96e0f0646296695806d
mitchellgsides <mitchellgsides@gmail.com>
2019-04-12 15:00:28 -0500

    initial commit

---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+  {
+    id: "1",
+    title: "book one",
+    url: "url goes right here.com",
+    description: "This is the first book. Man is it a good book",
+    rating: 5
+  }
+];
+
+module.exports = bookmarks;
